8ad422e542f20fe5442b736dbee80ed9aae847d3
renzoserra <82285676+renzoserra@users.noreply.github.com>
2024-07-02 12:23:07 -0400

    Exectute for githubActions

--- src/main/java/steps/Hooks.java
+++ src/main/java/steps/Hooks.java
@@ -11,20 +11,13 @@ import utils.browser_manager.WebDriverFactory;
 public class Hooks {
     public static WebDriver driver;
     public static String Tag_ID_Running;
-    String browser = "chrome";
+    String browser = "chromegithubactions";
     @Before
     public void setup() {
-//        String browser = System.getProperty("browser", "chrome"); // Por defecto, usa Chrome
+        String browser = System.getProperty("browser", "chrome"); // Por defecto, usa Chrome
         driver = WebDriverFactory.createWebDriver(browser);
         driver.manage().window().maximize();
     }
-//    @AfterStep
-//    public static void takeScreenshot(Scenario scenario) {
-//        if (driver instanceof TakesScreenshot) {
-//            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-//            scenario.attach(screenshot, "image/png","image" );
-//        }
-//    }
     @Before
     public void beforeScenario(Scenario scenario) {
         Tag_ID_Running = obtenerTagDelEscenario(scenario);
